e652cecb12fc3efc3f2e31389a79cdbace9a51dc
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-21 21:24:54 +0100

    Add Custom Error class

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import "./loadEnvironment.js";
 import chalk from "chalk";
 import debugCreator from "debug";
 import { mongo } from "mongoose";
-import app from "./server/app.js";
 import startServer from "./server/index.js";
+import app from "./server/app.js";
 import environment from "./loadEnvironment.js";
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const { MongoServerError } = mongo;
 
