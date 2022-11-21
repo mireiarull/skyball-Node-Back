6bd5cd42bf05faa9ce1e864fa3c1c45c969d8f6b
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-21 20:43:40 +0100

    Add app file in server and initialize server from index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+import "./loadEnvironment.js";
+import chalk from "chalk";
+import debugCreator from "debug";
+import { mongo } from "mongoose";
+import app from "./server/app.js";
+import startServer from "./server/index.js";
+import environment from "./loadEnvironment.js";
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const { MongoServerError } = mongo;
 
+const debug = debugCreator("skyball: server: root");
+
+try {
+  await startServer(app, environment.port);
+  debug(
+    chalk.yellow(`Server listening on: http://localhost:${environment.port}`)
+  );
+} catch (error: unknown) {
+  if (error instanceof MongoServerError) {
+    debug(
+      chalk.red(`Error connecting to the database ${(error as Error).message}`)
+    );
+  } else {
+    debug(chalk.red(`Error starting the server ${(error as Error).message}`));
+  }
+}
