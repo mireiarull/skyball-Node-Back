acbe0a5d90d68f9b69b1340b63933953fbcbd906
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-22 10:57:34 +0100

    Add user register joi schema

---
+++ src/server/controllers/schemas/userCredentialsSchema.ts
@@ -0,0 +1,11 @@
+import { Joi } from "express-validation";
+
+export const userRegisterSchema = {
+  body: Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().email().required(),
+    name: Joi.string().required(),
+    gender: Joi.string().required(),
+    lever: Joi.number().required(),
+  }),
+};
