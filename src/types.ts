13b65c96ee12f14ce4300f706a9c0560e8a1e9b4
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-24 10:02:37 +0100

    Add auth middleware

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import type { JwtPayload } from "jsonwebtoken";
+import type { Request } from "express";
 
+export interface CustomRequest extends Request {
+  userId: string;
+}
 export interface UserTokenPayload extends JwtPayload {
   id: string;
   email: string;
