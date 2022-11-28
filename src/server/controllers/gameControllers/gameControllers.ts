82299ca9a32c1b9a386d7f307c74196a7c2dd433
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-28 12:07:55 +0100

    Add create on game controller function

--- src/server/controllers/gameControllers/gameControllers.ts
+++ src/server/controllers/gameControllers/gameControllers.ts
@@ -1,6 +1,10 @@
 import type { NextFunction, Request, Response } from "express";
 import CustomError from "../../../CustomError/CustomError.js";
+import debugCreator from "debug";
+import type { GameStructure } from "../../../database/models/Game.js";
 import Game from "../../../database/models/Game.js";
+
+const debug = debugCreator("skyball: controllers: games");
 
 export const getAllGames = async (
   req: Request,
@@ -17,6 +21,29 @@ export const getAllGames = async (
       500,
       "Database error"
     );
+    next(customError);
+  }
+};
+
+export const addOneGame = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const game = req.body as GameStructure;
+
+  try {
+    const newGame = await Game.create(game);
+
+    res.status(201).json(newGame);
+  } catch (error: unknown) {
+    debug((error as Error).message);
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Error saving game"
+    );
+
     next(customError);
   }
 };
